ef9c7571ab22d4334b50f6bccaba71853f13aac1
rudrakri <krishna.kug070@gmail.com>
2024-04-15 23:33:58 +0530

    add splashScreen
    splash.js for more info.

---
+++ GestureVoice/screens/splash.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { View, StyleSheet, Dimensions} from 'react-native';
+import { Video, ResizeMode } from 'expo-av';
+
+const { width, height } = Dimensions.get('window');
+
+export default function Splash() {
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
+
+  React.useEffect(() => {
+    // Play the video once the component mounts
+    if (video && video.current) {
+      video.current.playAsync();
+    }
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Video
+        ref={video}
+        style={styles.video}
+        source={require('../assets/splash.mp4')}
+        useNativeControls
+        resizeMode={ResizeMode.COVER}
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  video: {
+    alignSelf: 'center',
+    width: width*1,
+    height: height*1.04,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
